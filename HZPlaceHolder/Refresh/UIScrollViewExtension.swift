98c88bd27f28388e8c304f5a98ec04496db12950
何志志 <951564399@qq.com>
2019-12-25 14:44:51 +0800

    Usage adjustment

--- HZPlaceHolder/Refresh/UIScrollViewExtension.swift
+++ HZPlaceHolder/Refresh/UIScrollViewExtension.swift
@@ -8,75 +8,30 @@
 
 import MJRefresh
 
+public typealias HZRefreshScrollView = UIScrollView
+
+public struct RefreshWrapper<Base> {
+    public let base: Base
+    public init(_ base: Base) {
+        self.base = base
+    }
+}
+
+public protocol RefreshCompatible: AnyObject { }
+
+extension RefreshCompatible {
+    public var hz: RefreshWrapper<Self> {
+        get { return RefreshWrapper(self) }
+        set { }
+    }
+}
+
+extension HZRefreshScrollView: RefreshCompatible { }
+
 extension UIScrollView {
     
-    //MARK: - normal刷新
-    /// 上下拉
-    public func hz_normalRefreshWithHeaderFooter<H: MJRefreshNormalHeader, F: MJRefreshFooter>(_ isFirstRefresh: Bool = false,
-                                                                                               refreshHeader: H.Type = HZRefreshNormalHeader.self as! H.Type,
-                                                                                               refreshFooter: F.Type = HZRefreshBackNormalFooter.self as! F.Type,
-                                                                                               headerRefreshHandler: @escaping () -> Void,
-                                                                                               footerRefreshHandler: @escaping () -> Void) {
-        self.setNormalRefresh(isFirstRefresh, refreshHeader: refreshHeader, refreshFooter: refreshFooter, headerRefreshHandler: headerRefreshHandler, footerRefreshHandler: footerRefreshHandler)
-    }
-    
-    /// 下拉
-    public func hz_normalRefreshWithHeader<H: MJRefreshNormalHeader>(_ isFirstRefresh: Bool = false,
-                                                                     refreshHeader: H.Type = HZRefreshNormalHeader.self as! H.Type,
-                                                                     headerRefreshHandler: @escaping () -> Void) {
-        self.setNormalRefresh(isFirstRefresh, refreshHeader: refreshHeader, headerRefreshHandler: headerRefreshHandler)
-    }
-    
-    /// 上拉
-    public func hz_normalRefreshWithFooter<F: MJRefreshFooter>(_ refreshFooter: F.Type = HZRefreshBackNormalFooter.self as! F.Type,
-                                                               footerRefreshHandler: @escaping () -> Void) {
-        self.setNormalRefresh(refreshFooter: refreshFooter, footerRefreshHandler: footerRefreshHandler)
-    }
-    
-    fileprivate func setNormalRefresh<H: MJRefreshNormalHeader, F: MJRefreshFooter>(_ isFirstRefresh: Bool = false,
-                                                                                    refreshHeader: H.Type? = nil,
-                                                                                    refreshFooter: F.Type? = nil,
-                                                                                    headerRefreshHandler: (() -> Void)? = nil,
-                                                                                    footerRefreshHandler: (() -> Void)? = nil) {
-        if let _refreshHeader = refreshHeader, let _headerRefreshHandler = headerRefreshHandler {
-            self.mj_header = _refreshHeader.init(refreshingBlock: {
-                _headerRefreshHandler()
-            })
-            if isFirstRefresh {
-                self.mj_header?.beginRefreshing()
-            }
-        }
-        if let _refreshFooter = refreshFooter, let _footerRefreshHandler = footerRefreshHandler {
-            self.mj_footer = _refreshFooter.init(refreshingBlock: {
-                _footerRefreshHandler()
-            })
-        }
-    }
-    
-    //MARK: - gif刷新
-    /// 上下拉
-    public func hz_gifRefreshWithHeaderFooter<H: MJRefreshGifHeader, F: MJRefreshBackGifFooter>(_ isFirstRefresh: Bool = false,
-                                                                                                refreshHeader: H.Type,
-                                                                                                refreshFooter: F.Type,
-                                                                                                headerRefreshHandler: @escaping () -> Void,
-                                                                                                footerRefreshHandler: @escaping () -> Void) {
-        self.setGifRefresh(isFirstRefresh, refreshHeader: refreshHeader, refreshFooter: refreshFooter, headerRefreshHandler: headerRefreshHandler, footerRefreshHandler: footerRefreshHandler)
-    }
-    
-    /// 下拉
-    public func hz_gifRefreshWithHeader<H: MJRefreshGifHeader>(_ isFirstRefresh: Bool = false,
-                                                               refreshHeader: H.Type,
-                                                               headerRefreshHandler: @escaping () -> Void) {
-        self.setGifRefresh(isFirstRefresh, refreshHeader: refreshHeader, headerRefreshHandler: headerRefreshHandler)
-    }
-    
-    /// 上拉
-    public func hz_gifRefreshWithFooter<F: MJRefreshBackGifFooter>(_ refreshFooter: F.Type,
-                                                                   footerRefreshHandler: @escaping () -> Void) {
-        self.setGifRefresh(refreshFooter: refreshFooter, footerRefreshHandler: footerRefreshHandler)
-    }
-    
-    fileprivate func setGifRefresh<H: MJRefreshGifHeader, F: MJRefreshBackGifFooter>(_ isFirstRefresh: Bool = false,
+    /// 公共方法
+    fileprivate func setRefresh<H: MJRefreshHeader, F: MJRefreshFooter>(_ isFirstRefresh: Bool = false,
                                                                                      refreshHeader: H.Type? = nil,
                                                                                      refreshFooter: F.Type? = nil,
                                                                                      headerRefreshHandler: (() -> Void)? = nil,
@@ -94,6 +49,91 @@ extension UIScrollView {
                 _footerRefreshHandler()
             })
         }
+    }
+    
+}
+
+public extension RefreshWrapper where Base: UIScrollView {
+    
+    
+    //MARK: - --------------------------------------------- normal刷新 ---------------------------------------------
+    /// normal下拉
+    func normalRefreshWithHeader<H: MJRefreshNormalHeader>(_ isFirstRefresh: Bool = false,
+                                                           refreshHeader: H.Type,
+                                                           headerRefreshHandler: @escaping () -> Void) {
+        base.setRefresh(isFirstRefresh, refreshHeader: refreshHeader, headerRefreshHandler: headerRefreshHandler)
+    }
+    
+    //MARK: 回弹底部Footer
+    /// normal下拉 + backNormal上拉
+    func normalRefreshWithHeaderBackFooter<H: MJRefreshNormalHeader, F: MJRefreshBackNormalFooter>(_ isFirstRefresh: Bool = false,
+                                                                              refreshHeader: H.Type,
+                                                                              refreshFooter: F.Type,
+                                                                              headerRefreshHandler: @escaping () -> Void,
+                                                                              footerRefreshHandler: @escaping () -> Void) {
+        base.setRefresh(isFirstRefresh, refreshHeader: refreshHeader, refreshFooter: refreshFooter, headerRefreshHandler: headerRefreshHandler, footerRefreshHandler: footerRefreshHandler)
+    }
+    
+    /// backNormal上拉
+    func normalRefreshWithBackFooter<F: MJRefreshBackNormalFooter>(_ refreshFooter: F.Type,
+                                                                   footerRefreshHandler: @escaping () -> Void) {
+        base.setRefresh(refreshFooter: refreshFooter, footerRefreshHandler: footerRefreshHandler)
+    }
+    
+    //MARK: 自动刷新Footer
+    /// normal下拉 + autoNormal上拉
+    func normalRefreshWithHeaderAutoFooter<H: MJRefreshNormalHeader, F: MJRefreshAutoNormalFooter>(_ isFirstRefresh: Bool = false,
+                                                                              refreshHeader: H.Type,
+                                                                              refreshFooter: F.Type,
+                                                                              headerRefreshHandler: @escaping () -> Void,
+                                                                              footerRefreshHandler: @escaping () -> Void) {
+        base.setRefresh(isFirstRefresh, refreshHeader: refreshHeader, refreshFooter: refreshFooter, headerRefreshHandler: headerRefreshHandler, footerRefreshHandler: footerRefreshHandler)
+    }
+    
+    /// autoNormal上拉
+    func normalRefreshWithAutoFooter<F: MJRefreshAutoNormalFooter>(_ refreshFooter: F.Type,
+                                                                   footerRefreshHandler: @escaping () -> Void) {
+        base.setRefresh(refreshFooter: refreshFooter, footerRefreshHandler: footerRefreshHandler)
+    }
+    
+    //MARK: - --------------------------------------------- gif刷新 ---------------------------------------------
+    /// gif下拉
+    func gifRefreshWithHeader<H: MJRefreshGifHeader>(_ isFirstRefresh: Bool = false,
+                                                           refreshHeader: H.Type,
+                                                           headerRefreshHandler: @escaping () -> Void) {
+        base.setRefresh(isFirstRefresh, refreshHeader: refreshHeader, headerRefreshHandler: headerRefreshHandler)
+    }
+    
+    //MARK: 回弹底部Footer
+    /// gif下拉 + backGif上拉
+    func gifRefreshWithHeaderBackFooter<H: MJRefreshGifHeader, F: MJRefreshBackGifFooter>(_ isFirstRefresh: Bool = false,
+                                                                              refreshHeader: H.Type,
+                                                                              refreshFooter: F.Type,
+                                                                              headerRefreshHandler: @escaping () -> Void,
+                                                                              footerRefreshHandler: @escaping () -> Void) {
+        base.setRefresh(isFirstRefresh, refreshHeader: refreshHeader, refreshFooter: refreshFooter, headerRefreshHandler: headerRefreshHandler, footerRefreshHandler: footerRefreshHandler)
+    }
+    
+    /// backGif上拉
+    func gifRefreshWithBackFooter<F: MJRefreshBackGifFooter>(_ refreshFooter: F.Type,
+                                                                   footerRefreshHandler: @escaping () -> Void) {
+        base.setRefresh(refreshFooter: refreshFooter, footerRefreshHandler: footerRefreshHandler)
+    }
+    
+    //MARK: 自动刷新Footer
+    /// gif下拉 + autoGif上拉
+    func gifRefreshWithHeaderAutoFooter<H: MJRefreshGifHeader, F: MJRefreshAutoGifFooter>(_ isFirstRefresh: Bool = false,
+                                                                              refreshHeader: H.Type,
+                                                                              refreshFooter: F.Type,
+                                                                              headerRefreshHandler: @escaping () -> Void,
+                                                                              footerRefreshHandler: @escaping () -> Void) {
+        base.setRefresh(isFirstRefresh, refreshHeader: refreshHeader, refreshFooter: refreshFooter, headerRefreshHandler: headerRefreshHandler, footerRefreshHandler: footerRefreshHandler)
+    }
+    
+    /// autoGif上拉
+    func gifRefreshWithAutoFooter<F: MJRefreshAutoGifFooter>(_ refreshFooter: F.Type,
+                                                                   footerRefreshHandler: @escaping () -> Void) {
+        base.setRefresh(refreshFooter: refreshFooter, footerRefreshHandler: footerRefreshHandler)
     }
     
 }
